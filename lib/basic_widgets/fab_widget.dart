46d181b699f374fe26d8feb23dcaf8d13348e4fd
aidatulrosida <144088067+aidatulrosida@users.noreply.github.com>
2025-10-01 10:43:06 +0700

    praktikum 4

---
+++ lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// This widget can be used as your main app or as a part of another widget tree.
+class FabWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          child: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
